76519ddff404e08a7a490e91f676bca774ef656e
mungji7 <brbr0525@gmail.com>
2023-08-17 02:03:34 +0900

    [0816 지은 ] 1. 모달 닫을때 url 변경 + 화면 변경 수정 완료 2. 스크립트 위치 이동 3. 본인의 송편 페이지면 링크 버튼 출력하고 본인의 송편 페이지가 아니면 송편 작성 버튼 출력하도록 변경 4. 에러페이지 고쳤는데 적용이 안됨..

--- daltokki/src/main/java/com/moon/daltokki/Controller/WebErrorController.java
+++ daltokki/src/main/java/com/moon/daltokki/Controller/WebErrorController.java
@@ -3,6 +3,7 @@ package com.moon.daltokki.Controller;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import javax.servlet.RequestDispatcher;
@@ -13,7 +14,7 @@ import javax.servlet.http.HttpServletRequest;
 public class WebErrorController implements ErrorController {
 
     @GetMapping("/error")
-    public String handleError(HttpServletRequest request) {
+    public String handleError(HttpServletRequest request, Model model) {
         Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
 
         if(status != null){
@@ -21,11 +22,16 @@ public class WebErrorController implements ErrorController {
 
             if(statusCode == HttpStatus.NOT_FOUND.value()) {
                 return "error/404";
+            } else if (statusCode == HttpStatus.BAD_REQUEST.value()) {
+                String message = "400 - 잘못된 요청이 있습니다.";
+                model.addAttribute("message", message);
+            } else if (statusCode == HttpStatus.METHOD_NOT_ALLOWED.value()) {
+                String message = "405 - 지원하지 않는 HTTP 메서드입니다.";
+                model.addAttribute("message", message);
             } else {
                 return "error/error";
             }
         }
-
         return "error/error";
     }
 
