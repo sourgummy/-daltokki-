f58badf08bbfd73f2408487bbe5835eb58d9b1fc
mungji7 <brbr0525@gmail.com>
2023-08-20 21:26:01 +0900

    [0820 지은 ] 1. 폰트 / css 변경 2. 카카오 url 수정

--- daltokki/src/main/java/com/moon/daltokki/Controller/UserController.java
+++ daltokki/src/main/java/com/moon/daltokki/Controller/UserController.java
@@ -4,6 +4,8 @@ import com.moon.daltokki.Model.UserModel;
 import com.moon.daltokki.Service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,11 +17,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 public class UserController {
     @Autowired
     UserService userService;
-
-    @GetMapping(value = "/")
-    public String index() {
-        return "main";
-    }
 
     @GetMapping(value = "/login") // 로그인 페이지
     public String login() {
@@ -40,5 +37,24 @@ public class UserController {
         userService.joinPro(user);
         return "redirect:/";
     }
+
+    // -----------------------------------------
+
+    // 첫화면
+    @GetMapping("/")
+    public String redirectToindex() {
+        return "main";
+    }
+
+    // 로그인시 사용자 식별 정보가 포함된 url 생성 - 지은
+    @GetMapping("/loginSuccess")
+    public String loginSuccessRedirect() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        String userId = auth.getName();
+
+        String mainUrl = "/main?id=" + userId;
+        return "redirect:" + mainUrl;
+    }
+    // -----------------------------------------
 
 }
