a3238844561bee312f1da0cb5a59b7624e4f3598
C-log0 <118327352+C-log0@users.noreply.github.com>
2023-09-03 14:57:50 +0900

    네이버로그인(토큰/정보까지)

--- daltokki/src/main/java/com/moon/daltokki/DaltokkiApplication.java
+++ daltokki/src/main/java/com/moon/daltokki/DaltokkiApplication.java
@@ -2,8 +2,9 @@ package com.moon.daltokki;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class DaltokkiApplication {
 
 	public static void main(String[] args) {
